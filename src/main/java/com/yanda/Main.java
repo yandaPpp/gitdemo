566ac537855eabf0eecaa3c07de2ce478d04977e
yanda <email@example.com>
2024-10-26 22:09:56 +0800

    master 2 commit

--- src/main/java/com/yanda/Main.java
+++ src/main/java/com/yanda/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Hello 3");
         System.out.println("Hello 4");
         System.out.println("Hello 5");
-
+        System.out.println("mast 6");
 
 
 
