720dfe6fdd1d541c07547ae5a275e0d653991e21
yanda <email@example.com>
2024-10-26 21:12:24 +0800

    third commit

--- src/main/java/com/yanda/Main.java
+++ src/main/java/com/yanda/Main.java
@@ -3,6 +3,7 @@ package com.yanda;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
